ae41e22bdc731c9e6a11babef5f9e8295d93968f
Yohan Boiroux <yohanboiroux@gmail.com>
2025-03-27 19:03:33 +0100

    mon deuxieme commit

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def hello(request):
+    return HttpResponse("<h1>Hello, Django!</h1>")
+
+def about(request):
+    return HttpResponse("<h1>About Us</h1>")
+
+def listings(request):
+    return HttpResponse("<h1>Listings</h1>")
+
+def contact(request):
+    return HttpResponse("<h1>Contact</h1>")
